71b79be9766fe458b38dab1069939d73b6de1eec
Eric Alejandro Magdaleno Martinez <90100623+AlejandroFFXV@users.noreply.github.com>
2024-06-03 11:29:32 -0600

    Update mysql.js

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,10 +2,10 @@ import mysql from "serverless-mysql";
 // Como conectar una base de datos con NextJS en src creamos la carpeta libs y en dicha carpeta nuestro archivo
 export const conn = mysql({
   config: {
-    host: "localhost",
-    user: "michosa",
-    password: "nerobb",
-    port: "3306",
-    database: "todoapp",
+    host: "Tus credenciales",
+    user: "Tus credenciales",
+    password: "Tus credenciales",
+    port: "Tus credenciales",
+    database: "Tus credenciales",
   },
 });
